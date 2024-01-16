88dc67c61537d7a37910642a25a0066917b1ef82
DenysLinetskyi <denys.linetskyi@dewais.com>
2024-01-16 16:41:04 +0200

    separate component with data

--- src/App.tsx
+++ src/App.tsx
@@ -3,155 +3,12 @@ import AccountsTable from './components/AccountsTable';
 import ProfilesTable from './components/ProfilesTable';
 import CampaignsTable from './components/CampaignsTable';
 import { useState } from 'react';
+import { sampleData, profilesData, campaignsData } from './data/data';
 
 const App = () => {
   const [selectedAccountId, setSelectedAccountId] = useState<string | null>(
     null
   );
-  const sampleData = [
-    {
-      accountId: '1',
-      email: 'matfraser@gmail.com',
-      authToken: 'xyz123',
-      creationDate: '2022-01-15',
-    },
-    {
-      accountId: '2',
-      email: 'richfroning@gmail.com',
-      authToken: 'xyz1234',
-      creationDate: '2022-01-16',
-    },
-    {
-      accountId: '3',
-      email: 'tiatoomey@gmail.com',
-      authToken: 'xyz12345',
-      creationDate: '2022-01-15',
-    },
-    {
-      accountId: '4',
-      email: 'davecastro@gmail.com',
-      authToken: 'xyz123456',
-      creationDate: '2022-01-16',
-    },
-    {
-      accountId: '5',
-      email: 'rickygarrard@gmail.com',
-      authToken: 'xyz123',
-      creationDate: '2022-01-15',
-    },
-    {
-      accountId: '6',
-      email: 'aleksandrusyk@gmail.com',
-      authToken: 'xyz1234',
-      creationDate: '2022-01-16',
-    },
-    {
-      accountId: '7',
-      email: 'anreuhoude@gmail.com',
-      authToken: 'xyz12345',
-      creationDate: '2022-01-15',
-    },
-    {
-      accountId: '8',
-      email: 'nike@gmail.com',
-      authToken: 'xyz123456',
-      creationDate: '2022-01-16',
-    },
-  ];
-
-  const profilesData = [
-    {
-      profileId: '1',
-      country: 'USA',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '2',
-      country: 'Ukraine',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '3',
-      country: 'UK',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '4',
-      country: 'Austria',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '5',
-      country: 'Germany',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '6',
-      country: 'Australia',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '7',
-      country: 'Swiss',
-      marketplace: 'Amazon',
-    },
-    {
-      profileId: '8',
-      country: 'Netherlands',
-      marketplace: 'Amazon',
-    },
-  ];
-
-  const campaignsData = [
-    {
-      campaignId: '1',
-      clicks: '100',
-      cost: '500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '2',
-      clicks: '110',
-      cost: '1500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '3',
-      clicks: '120',
-      cost: '2500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '4',
-      clicks: '130',
-      cost: '3500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '5',
-      clicks: '140',
-      cost: '4500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '6',
-      clicks: '150',
-      cost: '5500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '7',
-      clicks: '160',
-      cost: '6500$',
-      date: '2022-01-15',
-    },
-    {
-      campaignId: '8',
-      clicks: '170',
-      cost: '7500$',
-      date: '2022-01-15',
-    },
-  ];
 
   const handleRowClick = (accountId: string) => {
     setSelectedAccountId(accountId);
@@ -209,7 +66,9 @@ const App = () => {
         />
         <Route
           path='/accounts/:accountId'
-          element={<Link to={`/accounts/${selectedAccountId}/profiles`} replace />}
+          element={
+            <Link to={`/accounts/${selectedAccountId}/profiles`} replace />
+          }
         />
         <Route
           path='/accounts/:accountId/profiles'
